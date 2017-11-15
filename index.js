304c6bf66eee51a6a04e630c1539e1d25274bfc1
Mikael Hallendal <hallski@hallski.org>
2017-11-15 21:31:48 +0100

    Final touch for blog post

--- index.js
+++ index.js
@@ -1,43 +1,27 @@
-function a(gen, value) {
+function iterateAsync(gen, { value, done }) {
+  if (done) {
+    return Promise.resolve(value)
+  }
+
   return new Promise((resolve, reject) => {
-    const next = gen.next(value)
-    if (!next.done) {
-      const v = next.value
-      const p = v instanceof Promise ? v : Promise.resolve(v)
-      return p.then(r => resolve(a(gen, r)))
-        .catch(error => gen.throw(error))
-    } else {
-      resolve(next.value)
-    }
+    const promise = value instanceof Promise ? value : Promise.resolve(value)
+    return promise
+      .then(result => resolve(iterateAsync(gen, gen.next(result))))
+      .catch(error => {
+        try {
+          resolve(iterateAsync(gen, gen.throw(error)))
+        } catch (newError) {
+          reject(newError)
+        }
+      })
   })
 }
 
-// generator -> function
-function asyncinator(generator) {
+function async(generator) {
   return function(...args) {
     const gen = generator(...args)
-    return a(gen)
+    return iterateAsync(gen, gen.next())
   }
 }
 
-function promiseFunc(timeout, value) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => reject(value), timeout)
-  })
-}
-
-const asyncFunc = asyncinator(function*(start) {
-  try {
-    const first = yield promiseFunc(1000, start)
-    const oneAndAHalf = yield first + 100
-    const second = yield promiseFunc(1000, oneAndAHalf + 200)
-
-    return second
-  } catch (error) {
-    console.error('catch in asyncinator', error)
-  }
-})
-
-asyncFunc(100)
-  .then(result => console.log(`Asyncinator func done with value: ${result}`))
-  .catch(error => console.error('error asyncinator:', error))
+module.exports = async
